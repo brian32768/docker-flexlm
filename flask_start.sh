cd26b249f452e80a7a82d95b42428b794ad36fc3
Brian H Wilson <brian@wildsong.biz>
2020-06-23 10:43:21 -0700

    added flask launcher

--- flask_start.sh
+++ flask_start.sh
@@ -5,7 +5,13 @@ LMHOME='C:/Program Files (x86)/ArcGIS/LicenseManager/bin'
 LMUTIL="lmutil.exe"
 LICENSE="service.txt"
 
-conda activate arctic
+export LMHOME
+export LMUTIL
+export LICENSE
+
+# Obviously this is not the right way to do this.
+#PYTHON=/c/Users/bwilson.CLATSOP/AppData/Local/ESRI/conda/envs/arctic/python
+PYTHON=python
 
 # I have no idea why my Windows Server instance needs the pwd here, freaky.
-python `pwd`/start_app.py
+$PYTHON `pwd`/start_app.py
