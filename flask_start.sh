acfbbda2d6bc9a85a9c1eedce1f20f904a02331a
Brian H Wilson <brian@wildsong.biz>
2020-06-23 10:10:45 -0700

    added flask launcher

---
+++ flask_start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+# This starts the flask app as a standalone on Windows
+
+LMHOME='C:/Program Files (x86)/ArcGIS/LicenseManager/bin'
+LMUTIL="lmutil.exe"
+LICENSE="service.txt"
+
+python app.py
